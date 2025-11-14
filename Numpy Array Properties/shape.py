010cf29a821d2e1b19978109c9a23eddcdeb15c3
Faraaz Ashraf <farazashraf1523@gmail.com>
2025-11-14 15:14:20 +0530

    Completed the Numpy Array properties like shape , size  , and dimension

---
+++ Numpy Array Properties/shape.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr_2D = np.array([[1, 2, 3], [4, 5, 6]])
+print(arr_2D)
+
+#! Shape of the array -> means how many rows and column is present
+print(f"Shape of the array is: {arr_2D.shape}")
+
+#! Size of the array -> how many elements is present inside the array
+print(f"Size of the array is : {arr_2D.size}")
+
+#! Dimension of the array :
+print(f"Dimension of the array is: {arr_2D.ndim}")
